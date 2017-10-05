045ee83a2746d8833524fa8211343d2047b14384
mazedicer <mazedicer@gmail.com>
2017-10-05 11:27:48 -0700

    block1 starting block1 design

---
+++ templates/block1.php
@@ -0,0 +1,16 @@
+<div class="ahsBuild"><div id="11343Edit" class="editBlock editHBlock"> <a href="http://preparecal.dev/wp-admin/post.php?post=11343&amp;action=edit"><img data-toggle="tooltip" data-placement="top" title="" src="http://preparecal.dev/wp-content/themes/persuader/assets/img/editblock.png" data-original-title="Edit Hero Block"></a></div><div class="CTAContainer"><div class="ahsContent hrFloat"><div class="CTAGroup" style="display:inline-block; vertical-align:middle;"><h2></h2><p></p></div></div></div></div>
+<div class="ahsMobile bottomEdgeContainer"><div class="CTAContainer"><div class="ahsContent hrFloat"><div class="CTAGroup" style="display:inline-block; vertical-align:middle;"><h2></h2><p></p></div></div></div></div>
+<div class="bodyContent">
+    <div class="container" style="padding:0 !important;"> 
+        <a class="anchorLink"></a>
+        <div class="mainContentBlock" style="background:transparent; position:relative; z-index:2; display:block; min-height:200px; margin-top: -15vh;">
+            <div class="gcbBG gcbBG13086 ">
+                <div class="container-fluid gcb sidekicks ID13086">
+                    <div class="row">
+                        <h1>WORKING</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
