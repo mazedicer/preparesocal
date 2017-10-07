18e724923cd047b8cb628991c34abade1f4b2f66
mazedicer <mazedicer@gmail.com>
2017-10-06 17:55:01 -0700

    footer finished footer layout

--- templates/footerCustom.php
+++ templates/footerCustom.php
@@ -12,13 +12,13 @@ $contact_us="";
 $mc_footer_template='<footer id="footerCustom">
                         <div class="container">
                             <div class="row">
-                                <div class="col-md-6 d-flex justify-content-center">
+                                <div id="footer-content-left" class="col-md-6 d-flex justify-content-right">
                                     <div class="footer-left-content text-white">
                                         <h3>Stay in the Know</h3>
                                         <p>Be on top of what is going on in your community.</p>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
+                                <div id="footer-content-right" class="col-md-6 d-flex justify-content-left">
                                     <div class="form-row align-items-center">
                                         <div class="form-row align-items-center">
                                             <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
