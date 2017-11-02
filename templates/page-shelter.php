f79a0e1d90d9cc9f0863739fa2ee21522ecf50e3
mazedicer <mazedicer@gmail.com>
2017-11-01 17:28:00 -0700

    shelter custom page template start

---
+++ templates/page-shelter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Shelter
+ *
+ * @package WordPress
+ * @subpackage DesignStudio
+ */
+include_once(get_template_directory().'/head.php' );
+//wp_head();
+
+   get_template_part('/templates/content-map');
+
+ //footer.php - includes the footer switcher and version
+include_once(get_template_directory().'/footer.php');
+//wp_footer();
+?>
