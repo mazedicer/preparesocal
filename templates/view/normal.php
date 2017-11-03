a0488fe8fb57495208c9a20c4ee28fb935d347e5
mazedicer <mazedicer@gmail.com>
2017-11-03 14:55:43 -0700

    google maps api info window styling, troubleshooting

--- templates/view/normal.php
+++ templates/view/normal.php
@@ -50,13 +50,54 @@
                 shelter_header.offset({top:dimh});
             }
         }
+
+        /*       //map location template
+                var description_template=`<div class="ds_wpgm_phone"><span class="ds_wpgm_li">Phone:</span> <p class="wpgmza_iw_address_p">{phone}</p></div>
+                                            <div class="ds_wpgm_capacity"><span class="ds_wpgm_li">Capacity:</span> <p class="wpgmza_iw_address_p">{capacity}</p></div>
+                                            <div class="ds_wpgm_status"><span class="ds_wpgm_li">Status:</span> <p class="wpgmza_iw_address_p">{status}</p></div>
+                                            <hr>
+                                            <h4>Shelter Services</h4>
+                                            <div class="shelter-services">{shelter_services}</div>`;
+                var open24_template=`<div class="shelter-service">
+                                        <img src="/wp-content/uploads/open-24-hours-signboard.png" alt="open 24 hours" />
+                                        <h6>Open 24 Hours</h6>
+                                            </div>`;
+                var firstaid_template=`<div class="shelter-service">
+                                        <img src="/wp-content/uploads/nurse-head.png" alt="first aid" />
+                                        <h6>First Aid</h6>
+                                            </div>`;
+                var snacks_template=`<div class="shelter-service">
+                                        <img src="/wp-content/uploads/bananas.png" alt="free snacks" />
+                                        <h6>Free Snacks</h6>
+                                            </div>`;
+                var shelter_services="";
+
+                
+                if(parsed_content.services.open24==1){
+                    shelter_services+=open24_template;
+                }
+                if(parsed_content.services.firstaid==1){
+                    shelter_services+=firstaid_template;
+                }
+                if(parsed_content.services.snacks==1){
+                    shelter_services+=snacks_template;
+                }
+                if(status=="FULL"){
+                    var new_status='<span class="text-danger">'+status+'</span>';
+                }else{
+                    var new_status=status;
+                }
+                var replace = ["{phone}","{capacity}","{status}","{shelter_services}"];
+                var replace_with = [phone,capacity,new_status,shelter_services];
+                var description=description_template.replaceArray(replace,replace_with);
+                $(".ds_wpgm_description").html(description);
+                $(".ds_wpgm_button").html(button);
+        */
     });
-</script>
-<script>
+
     ////////////////////////////////////////////////////////////////////
     // https://developers.google.com/maps/documentation/javascript/examples/geocoding-simple
     ////////////////////////////////////////////////////////////////////
-
     function initMap() {
         var map_style=[ { "featureType": "administrative", 
                          "elementType": "labels", 
@@ -137,10 +178,17 @@
             var bounds = new google.maps.LatLngBounds();
             // For loop that runs through the info on markersData making it possible to createMarker function to create the markers
             for (var i=0; i<addresses.length; i++){
-                var latlng=new google.maps.LatLng(addresses[i]['Latitude'], addresses[i]['Longitude']);
-                var title = addresses[i]['Title'];
-                var address = addresses[i]['Address'];
-                createMarker(latlng,title,address);
+                var id=addresses[i]['ID'];
+                var lat=addresses[i]['Latitude'];
+                var lng=addresses[i]['Longitude'];
+                var latlng=new google.maps.LatLng(lat,lng);
+                var title=addresses[i]['Title'];
+                var address=addresses[i]['Address'];
+                var phone_number=addresses[i]['Phone Number'];
+                var status=addresses[i]['Status'];
+                var capacity=addresses[i]['Capacity'];
+                var services=addresses[i]['Services'];
+                createMarker(id,latlng,title,address,phone_number,status,capacity,services,lat,lng);
                 // Marker’s Lat. and Lng. values are added to bounds variable
                 bounds.extend(latlng); 
             }
@@ -149,7 +197,7 @@
             map.fitBounds(bounds);
         }
         // This function creates each marker
-        function createMarker(latlng, title, address){
+        function createMarker(id,latlng,title,address,phone_number,status,capacity,services,lat,lng){
             var marker = new google.maps.Marker({
                 map: map,
                 position: latlng,
@@ -160,7 +208,7 @@
             // When this event is fired the infowindow content is created
             // and the infowindow is opened
             google.maps.event.addListener(marker, 'click', function() {
-                console.log(title+" "+address);
+                setInfoContent(id,title,address,phone_number,status,capacity,services,lat,lng);
                 /* Variable to define the HTML content to be inserted in the infowindow
                 var iwContent = '<div id="iw_container">' +
                     '<div class="iw_title">' + name + '</div>' +
@@ -173,6 +221,67 @@
                 infoWindow.open(map, marker);*/
             });
         }
+        function setInfoContent(id,title,address,phone_number,status,capacity,services,lat,lng){
+            $(".ds_wpgm_title").html(title);
+            $(".ds_wpgm_address").html('<span class="ds_wpgm_li">Address:</span> '+address);
+             //map location template
+                var description_template=`<div class="ds_wpgm_phone"><span class="ds_wpgm_li">Phone:</span> <p class="wpgmza_iw_address_p">{phone}</p></div>
+                                            <div class="ds_wpgm_capacity"><span class="ds_wpgm_li">Capacity:</span> <p class="wpgmza_iw_address_p">{capacity}</p></div>
+                                            <div class="ds_wpgm_status"><span class="ds_wpgm_li">Status:</span> <p class="wpgmza_iw_address_p">{status}</p></div>
+                                            <hr>
+                                            <h4>Shelter Services</h4>
+                                            <div class="shelter-services">{shelter_services}</div>`;
+                var open24_template=`<div class="shelter-service">
+                                        <img src="/wp-content/uploads/open-24-hours-signboard.png" alt="open 24 hours" />
+                                        <h6>Open 24 Hours</h6>
+                                            </div>`;
+                var firstaid_template=`<div class="shelter-service">
+                                        <img src="/wp-content/uploads/nurse-head.png" alt="first aid" />
+                                        <h6>First Aid</h6>
+                                            </div>`;
+                var snacks_template=`<div class="shelter-service">
+                                        <img src="/wp-content/uploads/bananas.png" alt="free snacks" />
+                                        <h6>Free Snacks</h6>
+                                            </div>`;
+                var button_template=`<a class="wpgmza_button wpgmza_left wpgmza_directions_button wpgmza_gd" gps="{lat},{lng}" href="javascript:void(0);" id="{id}" wpgm_addr_field="{address}">Get Directions</a>`;
+                var shelter_services="";
+
+                if(Array.isArray(services)){
+                    if(services.indexOf('open_twentyfour')>=0){
+                        shelter_services+=open24_template;
+                    }
+                    if(services.indexOf('first_aid')>=0){
+                        shelter_services+=firstaid_template;
+                    }
+                    if(services.indexOf('snacks')>=0){
+                        shelter_services+=snacks_template;
+                    }
+                }
+                
+                if(status=="full"){
+                    var new_status='<span class="text-danger">FULL</span>';
+                }else{
+                    var new_status='SPACE AVAILABLE!';
+                }
+                //description
+                var replace = ["{phone}","{capacity}","{status}","{shelter_services}"];
+                var replace_with = [phone_number,capacity,new_status,shelter_services];
+                var description=description_template.replaceArray(replace,replace_with);
+                $(".ds_wpgm_description").html(description);
+                //button
+                var replace_button = ["{lat}","{lng}","{id}","{address}"];
+                var replace_with_button = [lat,lng,id,address];
+                var dir_button=button_template.replaceArray(replace_button,replace_with_button);
+                $(".ds_wpgm_button").html(dir_button);
+        }
     }
+    //utility function to replace content using two arrays
+        String.prototype.replaceArray = function(find, replace) {
+            var replaceString = this;
+            for (var i = 0; i < find.length; i++) {
+                replaceString = replaceString.replace(find[i], replace[i]);
+            }
+            return replaceString;
+        }
 </script>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA15oypl4nlP5eu4Rq9S-UMrYCuBNny9C8&callback=initMap"></script>
